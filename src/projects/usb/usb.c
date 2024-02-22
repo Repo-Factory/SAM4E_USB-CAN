c94d6e83127fb0ac00af442fc6319d0a49df9f86
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-02-21 21:37:36 -0800

    Yay it works

--- src/projects/usb/usb.c
+++ src/projects/usb/usb.c
@@ -1,67 +1,88 @@
-/* USB Device Port
-The device uses the USB communication device class (CDC) drivers to take advantage of the installed PC RS-232
-software to talk over the USB. The CDC class is implemented in all releases of Windows ® , beginning with
-Windows 98SE. The CDC document, available at www.usb.org, describes a way to implement devices such as
-ISDN modems and virtual COM ports.
-The Vendor ID (VID) is Atmel’s vendor ID 0x03EB. The product ID (PID) is 0x6124. These references are used by
-the host operating system to mount the correct driver. On Windows systems, the INF files contain the
-correspondence between vendor ID and product ID.
-Host Device
-SOH 01 FE Data[128] CRC CRC
-C
-ACK
-SOH 02 FD Data[128] CRC CRC
-ACK
-SOH 03 FC Data[100] CRC CRC
-ACK
-EOT
-ACK
-447SAM4E Series [DATASHEET]
-Atmel-11157H-ATARM-SAM4E16-SAM4E8-Datasheet_31-Mar-16
-For more details about VID/PID for End Product/Systems, please refer to the Vendor ID form available from the
-USB Implementers Forum on www.usb.org.
-Atmel provides an INF example to see the device as a new serial port and also provides another custom driver
-used by the SAM-BA application: atm6124.sys. Refer to the application note “USB Basic Application”, Atmel
-literature number 6123, for more  */
-
-/* 
-//    iram_size_t udi_cdc_read_buf(int* buf, iram_size_t size);
-//    int udi_cdc_putc(int value);
-//    iram_size_t udi_cdc_write_buf(const int* buf, iram_size_t size);  */
-
 #include <asf.h>
 #include <stdio.h>
 #include "quick_blink.h"
 #include "led_f.h"
 
-static bool my_flag_autorize_cdc_transfert = false;
+volatile static char* this_works = "This shit works bitches. ";
+
+volatile static bool usb_enabled = false;
 bool my_callback_cdc_enable(void)
 {
-    my_flag_autorize_cdc_transfert = true;
-    return true;
+    return usb_enabled = true;
 }
 void my_callback_cdc_disable(void)
 {
-    my_flag_autorize_cdc_transfert = false;
+    usb_enabled = false;
+}
+
+#define PERIOD 1000
+volatile static int tickcount=0;
+void SysTick_Handler(void)
+{
+	if (++tickcount==100)
+	{
+        if (usb_enabled) 
+        {
+            if (*this_works != '\0')
+            {
+                const int val = udi_cdc_putc(*this_works);
+                this_works++;
+            } 
+            else
+            {
+                this_works = "This shit works bitches. "; // Reset pointer to beginning of the string
+            }
+            TOGGLE_LIGHT();
+        }
+		tickcount = 0;
+	}
+}
+
+void toggle_on_char(const char ch)
+{
+    int cdc_line_val = udi_cdc_getc();
+    if (cdc_line_val == ch) 
+        TOGGLE_LIGHT();
+}
+
+void usb_init(void)
+{
+    udc_start();
+    udi_cdc_comm_enable();
+    udi_cdc_data_enable();
+}
+
+void systick_init(void)
+{
+    SysTick_Config(SystemCoreClock/PERIOD);
+    NVIC_EnableIRQ(SysTick_IRQn);
+}
+
+void interrupt_init(void)
+{
+    irq_initialize_vectors();
+    cpu_irq_enable();
 }
 
 void hardware_init(void)
 {
     sysclk_init();
+    interrupt_init();
     board_init();
-    cpu_irq_enable();
-    udc_start();
+    usb_init();
+    systick_init();
+}
+
+void enter_loop(void)
+{
+    for (;;)
+    {
+        toggle_on_char('A');
+    }
 }
 
 int main(void) 
 {
     hardware_init();
-    for (;;)
-    {
-        if (my_flag_autorize_cdc_transfert) 
-        {
-            udi_cdc_putc('A');
-            udi_cdc_getc();
-        }
-    }
+    enter_loop();
 }
--- src/utils/led_f.h
+++ src/utils/led_f.h
@@ -3,9 +3,17 @@
 
 #include "asf.h"
 
+static uint32_t light_on=0;
+
 #define LED0_PIO    		   PIOD
 #define LED0_PIN    		   PIO_PD22
 #define SET_LIGHT_ON()         LED0_PIO->PIO_CODR = LED0_PIN
 #define SET_LIGHT_OFF()        LED0_PIO->PIO_SODR = LED0_PIN
+#define TOGGLE_LIGHT() \
+                               if (light_on) \
+                                   SET_LIGHT_OFF(); \
+                               else \
+                                   SET_LIGHT_ON(); \
+                               light_on = !light_on; \
 
 #endif
