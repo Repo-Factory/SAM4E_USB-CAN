0c25bd2d0b14af62d36f725c8386a853e5565db1
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-02-22 22:38:07 -0800

    Add to Protocol

--- src/projects/usb/include/usb_mech.h
+++ src/projects/usb/include/usb_mech.h
@@ -1,4 +1,5 @@
-#include <cstdint>
+#ifndef USB_MECH_H
+#define USB_MECH_H
 
 #define LONG_MESSAGE_SIZE 64
 #define SHORT_MESSAGE_SIZE 32
@@ -8,7 +9,7 @@
 #define FLAG_BYTES 2
 #define RESERVED_BYTES 2
 #define NON_DATA_BYTES TOPIC_BYTES+SUBTOPIC_BYTES+RESERVED_BYTES+FLAG_BYTES
-#define LONG_MSG_DATA LONG_MESSAGE_SIZE - NON_DATA_BYTES
+#define LONG_MSG_DATA LONG_MESSAGE_SIZE-NON_DATA_BYTES
 
 #define NUM_TOPICS 6
 #define NUM_SUBTOPICS 6
@@ -22,18 +23,19 @@ typedef struct long_message_t
     unsigned char reserved    [RESERVED_BYTES];
 } long_message_t;
 
-typedef enum device_category_t
+typedef enum device_topic_t
 {
-    CONTROLLER,
-    SYSTEM_MONITOR,
-    POWER_SYSTEM,
-    MOTION_CONTROL,
-    RESERVED,
-    AUXILLARY
-} device_category_t;
+    CONTROLLER      = 0,
+    SYSTEM_MONITOR  = 1,
+    POWER_SYSTEM    = 2,
+    MOTION_CONTROL  = 3,
+    RESERVED        = 4,
+    AUXILLARY       = 5
+} device_topic_t;
 
-// typedef enum device_topic_t
-// {
+/* 
+ * Will receive raw buffer from SAM
+ */
+void HANDLE_MESSAGE(const unsigned char* message);
 
-// } device_topic_t;
-
+#endif
--- src/projects/usb/src/usb_mech.c
+++ src/projects/usb/src/usb_mech.c
@@ -1,3 +1,11 @@
+/* 
+ * @name USB Mechatronics Protocol SAM4E
+ * @author Zix
+ * @date Feb 22 2024
+ * 
+ * @brief Defines Message Handling for USB Messages sent to SAM4E
+ */
+
 #include "usb_mech.h"
 
 #define BYTE_MASK 0xFF
@@ -6,8 +14,9 @@
 
 /* 
  * @brief Handler functions will go here. To perform specific function, deference this array at the appropriate topic/subtopic index
+ *        The data field should be passed as a parameter to these functions
  */
-const void (*topics[NUM_TOPICS][NUM_SUBTOPICS])(const unsigned char* data) =
+const void (*robot_action[NUM_TOPICS][NUM_SUBTOPICS])(const unsigned char* data) =
 {
 //     {topic1_subtopic1, topic1_subtopic2},
 //     {topic2_subtopic1, topic2_subtopic2}
@@ -15,9 +24,9 @@ const void (*topics[NUM_TOPICS][NUM_SUBTOPICS])(const unsigned char* data) =
 
 /*
  * @brief place appropriate values of message into corresponding field of long_message_t struct
- * @param field - pass in an empty struct of long_message_t and the answer will be "returned" here  
+ * @param field - pass in an empty struct of long_message_t and the answers will be "returned" here  
  */
-void extract_fields(const unsigned char* message, long_message_t* fields, const int message_size) 
+void extract_fields(const unsigned char* message, long_message_t* fields) 
 {
     for (int i = 0; i < TOPIC_BYTES; i++) {
         fields->topic_id[i] = message[i];
@@ -37,25 +46,26 @@ void extract_fields(const unsigned char* message, long_message_t* fields, const 
 }
 
 /*
- * @brief compose an array value into integer value with bit manipulations
+ * @brief compose an array value into integer value for comparisons
  * @param field - pass in member of long_message_t struct here
  */
 int extract_field_value(const unsigned char* field, const int field_size)
 {
     int field_value = 0;
     for (int i = 0; i < field_size; i++) {
-        field_value |= field[i] << (i * BYTE_SIZE);
+        const int shifted_byte = field[i] << (i * BYTE_SIZE);
+        field_value |= shifted_byte;
     }
     return field_value;
 }
 
-void HANDLE_MESSAGE(const unsigned char* message, const int message_size)
+void HANDLE_MESSAGE(const unsigned char* message)
 {
     long_message_t fields;
-    extract_fields(message, &fields, message_size);
+    extract_fields(message, &fields);
     const int topic = extract_field_value(fields.topic_id, TOPIC_BYTES);
     const int subtopic = extract_field_value(fields.subtopic_id, SUBTOPIC_BYTES);
     const int flags = extract_field_value(fields.flags, FLAG_BYTES);
     const int reserved = extract_field_value(fields.reserved, RESERVED_BYTES);
-    topics[topic][subtopic](fields.data);
+    robot_action[topic][subtopic](fields.data);
 }
